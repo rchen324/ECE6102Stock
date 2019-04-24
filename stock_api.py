8a4cb44ce96fd8f894ebe1aa1ed85daa3f73551a
陈润锋 <chenrunfeng@lawn-128-61-45-123.lawn.gatech.edu>
2019-04-24 15:01:11 -0400

    modified stock api

--- stock_api.py
+++ stock_api.py
@@ -1,42 +1,18 @@
-"""
-Author: Runfeng Chen
-
-There are only two functions that the server should use directly: clear_files() and prediction_today()
-
-Sample usage:
-    1, when the directory does not contain pre-generated graphs and data csv for today, do:
-            prediction_graph_name, training_graph_name, max, min, avg, trend = prediction_today("MSFT")
-
-    2, when the directroty contains pre-generated graphs and data csv for today, but you want the model to be retrained, do:
-            clear_files("MSFT")
-            prediction_graph_name, training_graph_name, max, min, avg, trend = prediction_today("MSFT")
-
-    3, when the directroty contains pre-generated graphs and data csv for today, but you want use those files again, do:
-            prediction_graph_name, training_graph_name, max, min, avg, trend = prediction_today("MSFT")
-
-Note: remember to install the required dependencies first
-"""
-
 import os
 import csv
 import time
-import numpy as np
 import datetime
 from datetime import date
-import matplotlib as mpl
-mpl.use("Agg")
-import matplotlib.pyplot as plt
 from alpha_vantage.timeseries import TimeSeries
-from sklearn.svm import SVR
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
 
 today = datetime.datetime.today()
 today_str = today.strftime("%Y-%m-%d")
 
 def stock_price(stock_name, days_num):
     avgs = []
+    row_num = days_num + 1
+    column_num = len(stock_name) + 1
+    avgs = [[0 for x in range(column_num)] for y in range(row_num)]
     for s in stock_name:
         print "Downloading data from alpha vantage for " + s
         today = datetime.datetime.today()
@@ -45,7 +21,6 @@ def stock_price(stock_name, days_num):
         for d in dateslist_datetime:
             d_str = d.strftime("%Y-%m-%d")
             dateslist.append(d_str)
-        #filename = "{}_data.csv".format(stock_name)
         ts = TimeSeries(key='OJC57JXH4SUQN338', output_format='csv')
         data = ts.get_daily(symbol=s)
         #write data to corresoinding csv file
@@ -54,13 +29,30 @@ def stock_price(stock_name, days_num):
         for row in data:
             for r in row or []:
                 if r[0].split(" ")[0] in dateslist:
-                    i += 1
-                    prices += (float(r[2]) + float(r[3])) / 2.0
-                    print r
-        avg = prices / float(i)
-        avgs.append((s, avg))
+                    row_id = dateslist.index(r[0].split(" ")[0]) + 1
+                    column_id = stock_name.index(s) + 1
+                    avg = (float(r[2]) + float(r[3])) / 2.0
+                    avgs[row_id][column_id] = avg
+                    avgs[row_id][0] = r[0].split(" ")[0]
         print "Downloading finished for " + s
+
+    print "Final prices are "
     print avgs
+    header = ["Date"] + stock_name
+    filename = "stocks/" + today_str + ".csv".format(stock_name)
+    print "\nWriting data to " + filename
+    data_file = open(filename, "wb")
+    writer = csv.writer(data_file, delimiter=',')
+    writer.writerow(header)
+
+    for avg_daily in avgs:
+        if not all(n == 0 for n in avg_daily):
+            writer.writerow(avg_daily)
+
+    print "Writing done"
+
+
+
 
 
 stock_price(["GOOGL", "FB", "MSFT"], 30)
