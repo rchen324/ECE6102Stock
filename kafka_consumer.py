0996e07a061e44e17352ba47f51de45a74453ab2
lyu <lvfanzhe@hotmail.com>
2019-04-24 17:56:35 -0400

    add new if

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -3,17 +3,19 @@ from kafka import KafkaConsumer
 import queue
 from portfolio import *
 from io import StringIO
+from google.cloud import datastore
+
 
 
 if __name__ == "__main__":
-    
+    datastore_client = datastore.Client()
     consumer = KafkaConsumer(TOPICS, bootstrap_servers='localhost:9092')
     q = queue.Queue()
     for msg in consumer:
         q.put(str(msg.value))
         if q.qsize() >= 30:
             q.get()
-        if q.qsize() >= 20:
+        if q.qsize() >= 21:
             head = 'date,GOOGL,FB,MSFT\n'
             for elem in list(q.queue):
                 s = str(elem)[3:-2]
@@ -22,6 +24,14 @@ if __name__ == "__main__":
             mu, S = calcMuCov2(StringIO(head))
             cleaned_weights, perf = maxSharpeRatio(mu, S)
             result = combineWeigPerf(cleaned_weights, perf)
+    
+            kind = 'Results'
+            name = 'result_'+today_str
+            task_key = datastore_client.key(kind, name)
+            task = datastore.Entity(key=task_key)
+            task['date'] = today_str
+            task['rec'] = str(result)
+            datastore_client.put(task)
             print (result)
 
                 
