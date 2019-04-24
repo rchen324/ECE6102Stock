21b8924a227d5f8d5a69eb07693a36e1a9f82e36
lyu <lvfanzhe@hotmail.com>
2019-04-24 16:37:01 -0400

    change stock api

--- stock_api.py
+++ stock_api.py
@@ -3,18 +3,12 @@ import csv
 import time
 import datetime
 from datetime import date
-<<<<<<< HEAD
-import matplotlib as mpl
-mpl.use("Agg")
-import matplotlib.pyplot as plt
-from kafka import KafkaProducer
-=======
->>>>>>> 7b6ee80940020e7b1197865eff89a1258cd439f4
 from alpha_vantage.timeseries import TimeSeries
+from kafka import KafkaProducer
 
 today = datetime.datetime.today()
 today_str = today.strftime("%Y-%m-%d")
-producer = KafkaProducer(bootstrap_servers='localhost:1234')
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 def stock_price(stock_name, days_num):
     avgs = []
@@ -31,21 +25,36 @@ def stock_price(stock_name, days_num):
             dateslist.append(d_str)
         ts = TimeSeries(key='OJC57JXH4SUQN338', output_format='csv')
         data = ts.get_daily(symbol=s)
-        print (data)
-        producer.send('foobar', b'some_message_bytes')
         #write data to corresoinding csv file
         i = 0
         prices = 0
         for row in data:
             for r in row or []:
                 if r[0].split(" ")[0] in dateslist:
-                    i += 1
-                    prices += (float(r[2]) + float(r[3])) / 2.0
-                    print (r)
-        avg = prices / float(i)
-        avgs.append((s, avg))
+                    row_id = dateslist.index(r[0].split(" ")[0]) + 1
+                    column_id = stock_name.index(s) + 1
+                    avg = (float(r[2]) + float(r[3])) / 2.0
+                    avgs[row_id][column_id] = avg
+                    avgs[row_id][0] = r[0].split(" ")[0]
         print ("Downloading finished for " + s)
+
+    print ("Final prices are ")
     print (avgs)
+    header = ["Date"] + stock_name
+    filename = "stocks/" + today_str + ".csv".format(stock_name)
+    print ("\nWriting data to " + filename)
+    data_file = open(filename, "w")
+    writer = csv.writer(data_file, delimiter=',')
+    # header_ = [bytes(h) for h in header]
+    print(header)
+    writer.writerow(header)
+    for avg_daily in avgs:
+        if not all(n == 0 for n in avg_daily):
+            # avg_daily = [str(a).encode for a in avg_daily]
+            writer.writerow(avg_daily)
+            producer.send("test", str(avg_daily).encode)
+            print(str(avg_daily))
+    print ("Writing done")
 
 if __name__ == "__main__":
     first = True
